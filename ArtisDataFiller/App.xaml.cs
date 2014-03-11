912c6cfd9219628d6a189916c4816a134b208a77
mlisakov <maksim.l.isakov@gmail.com>
2014-03-11 20:01:33 +0400

    Обработка необработанных исключений

--- ArtisDataFiller/App.xaml.cs
+++ ArtisDataFiller/App.xaml.cs
@@ -1,14 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace ArtisDataFiller
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+           // AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        //void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        //{
+        //    Exception ex = e.ExceptionObject as Exception;
+        //    MessageBox.Show(ex.Message, "Uncaught Thread Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+        //}
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+                //Handling the exception within the UnhandledExcpeiton handler.
+                MessageBox.Show(e.Exception.Message, "Exception Caught", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            //}
+            //else
+            //{
+            //    //If you do not set e.Handled to true, the application will close due to crash.
+            //    MessageBox.Show("Application is going to close! ", "Uncaught Exception");
+            //    e.Handled = false;
+            //}
+        }
     }
 }
