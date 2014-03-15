05de2cafcb55b705f14fedae30f63668ab07ea3a
mlisakov <maksim.l.isakov@gmail.com>
2014-03-15 12:15:27 +0400

    Добавлена возможность делать publish в xbap формат.адрес доступа по web 92.53.105.145:8200

---
+++ WebClient/ArtisAdministrativeToolWeb/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Artis.ArtisDataFiller
+{ 
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ShowLoginScreen();
+        }
+        private void ShowLoginScreen()
+        {
+            if (MainWindow == null) MainWindow = new Window();
+            MainWindow.Content = new MainWindow();
+            MainWindow.Show();
+        }
+    }
+
+}
